3d3639883f91dbe18a25aecff550f9f7202eff81
matt <mamurazizovich@gmail.com>
2026-09-21 09:57:10 +0900

    MITASK-M

--- train.ts
+++ train.ts
@@ -1,11 +1,28 @@
+// MITASK-M
+interface SquareResult {
+  number: number;
+  square: number;
+}
+
+function getSquareNumbers(arr: number[]): SquareResult[] {
+  let arrNums: SquareResult[] = arr.map((num: number) => {
+    return {number: num, square: num * num}
+  });
+  return arrNums;
+}
+
+console.log(getSquareNumbers([4, 5]))
+
 // MITASK-L
 function reverseSentence(sentence: string): string {
-    let words: string[] = sentence.split(' ');
-    let reversedWords: string[] = words.map((item: string) => {
-        return item.split('').reverse().join('')
-    })
-    let stringify: string = reversedWords.join(' ')
-    return stringify;
+  let words: string[] = sentence.split(" ");
+  let reversedWords: string[] = words.map((item: string) => {
+    return item.split("").reverse().join("");
+  });
+  let stringify: string = reversedWords.join(" ");
+  return stringify;
 }
 
 // console.log(reverseSentence("hi there"));
+
+
